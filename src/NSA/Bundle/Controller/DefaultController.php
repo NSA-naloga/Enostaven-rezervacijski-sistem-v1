46821f51949e30602071dd8450b00a95ecf3e6ff
JuiceN2 <javidic@gmail.com>
2015-03-17 00:55:16 -0700

    Bare project + database + fosuserbundle

---
+++ src/NSA/Bundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NSA\Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('NSABundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/NSA/Bundle/NSABundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NSA\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NSABundle extends Bundle
+{
+}
