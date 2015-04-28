d43d10655170af0919a499f755327603a784cfcf
JuiceN2 <javidic@gmail.com>
2015-04-28 12:47:15 -0700

    Sample - naloga

--- src/NSA/Bundle/Controller/DefaultController.php
+++ src/NSA/Bundle/Controller/DefaultController.php
@@ -15,4 +15,30 @@ class DefaultController extends Controller
 
     	return $this->render('NSABundle:Default:bootstrap_testing.html.twig');
     }
+
+
+	public function pokaziNalogoAction($id) {
+      $naloga = $this->getDoctrine()
+            ->getRepository('NSABundle:Naloga')
+            ->find($id);
+      if (!$naloga) {
+        throw $this->createNotFoundException('Naloga z ID-jem ' . $id.' ne obstaja');
+      }
+    
+      $build['naloga_item'] = $naloga;
+      return $this->render('NSABundle:Default:pokazi_nalogo.html.twig', $build);
+    }
+
+   public function vseNalogeAction(){
+   		$naloga = $this->getDoctrine()
+            ->getRepository('NSABundle:Naloga')
+            ->findAll();
+      if (!$naloga) {
+        throw $this->createNotFoundException('Ni najdenih nalog.');
+      }
+    
+      $build['naloga'] = $naloga;
+      return $this->render('NSABundle:Default:vseNaloge.html.twig', $build);
+   } 
+
 }
