cb6d1fb91665d7e6e219f1bf6499acac7b828c65
JuiceN2 <javidic@gmail.com>
2015-04-30 01:22:06 -0700

    Uporabniki,forma za nalogo

--- src/NSA/Bundle/Controller/DefaultController.php
+++ src/NSA/Bundle/Controller/DefaultController.php
@@ -3,12 +3,20 @@
 namespace NSA\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use NSA\Bundle\Entity\Naloga;
+use NSA\Bundle\Entity\Profesor;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name){
+    public function prvaAction(){
 
-        return $this->render('NSABundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('NSABundle:Default:index.html.twig');
+    }
+
+    public function successAction(){
+
+        return $this->render('NSABundle:Default:success.html.twig');
     }
 
     public function testingAction(){
@@ -40,5 +48,33 @@ class DefaultController extends Controller
       $build['naloga'] = $naloga;
       return $this->render('NSABundle:Default:vseNaloge.html.twig', $build);
    } 
+
+   public function dodajNalogoAction(Request $request){
+    $Naloga=new Naloga();
+
+    $form=$this->createFormBuilder($Naloga)
+        ->add('naslov','text')
+        ->add('opis','textarea')
+        ->add('kljucnebesede','textarea')
+        ->add('steviloKandidatov','integer')
+        ->add('zacetniDatum','datetime')
+        ->add('datumOddaje','date',array('years'=>range(date('Y'),date('Y')+2)))
+		->add('save','submit')
+        ->getForm();
+    $Naloga->setdatumKreiranja(new \DateTime()); // Sam doda datum dodajanja
+    $Naloga->setdatumObjave(new \DateTime()); // Sam doda datum dodajanja
+    $Naloga->setProfesor('1'); // SAMO ZA TEST
+    $form->handleRequest($request);
+    if($form->isValid()){
+      $em=$this->getDoctrine()->getManager();
+      $em->persist($Naloga);
+      $em->flush();
+      return $this->redirect($this->generateUrl('nsa_success'));
+    }
+    //$build['form']= $form->createView();
+     return $this->render('NSABundle:Default:dodaj_nalogo.html.twig',array('form' => $form->createView()));
+ 
+}
+
 
 }
